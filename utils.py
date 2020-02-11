46d2ec6ea056d7af86446bc0d1eeeec46aedb993
Beranger GUEDOU <rodrigueguedou2@gmail.com>
2020-02-11 16:48:43 +0100

    preprocessing and data loader

--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@ import os
 import io
 import re
 import sys
-import unicodedata
-from tqdm import tqdm
+import random
 import requests
+import unicodedata
+import tensorflow as tf
+from tqdm import tqdm
 from zipfile import ZipFile
 
 
@@ -58,12 +60,46 @@ def preprocess_sentence(w):
     return w
 
 
-# 1. Remove the accents
-# 2. Clean the sentences
-# 3. Return word pairs in the format: [ENGLISH, SPANISH]
+
 def create_dataset(path, num_examples):
+    # 1. Remove the accents
+    # 2. Clean the sentences
+    # 3. Return word pairs in the format: [ENGLISH, SPANISH]
     lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
 
     word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
-
+    random.shuffle(word_pairs)
     return zip(*word_pairs)
+
+
+def max_length(tensor):
+      return max(len(t) for t in tensor)
+
+    
+def tokenize(lang):
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+      filters='')
+    lang_tokenizer.fit_on_texts(lang)
+
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+                                                         padding='post')
+
+    return tensor, lang_tokenizer
+
+
+def load_dataset(path, num_examples=None):
+    # creating cleaned input, output pairs
+    targ_lang, inp_lang = create_dataset(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+def convert(lang, tensor):
+    for t in tensor:
+        if t!=0:
+            print ("%d ----> %s" % (t, lang.index_word[t]))
+  
